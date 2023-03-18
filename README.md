8ec5b9169964715dc69b0c2ab21aa0301ead0633
Sander <mail@sanderboer.nl>
2023-03-18 15:53:53 +0100

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Smartsheet bot
+
+Do you also hate to fill in your pre-set hours manually? Stop being a monkey, start doing important work.
+
+## What is this?
+
+An automated bot that will fill in your hours for you using github actions and playwright.
+
+## How does it work?
+
+1. Runs a cronjob every week
+2. Logs in to smartsheet and fills in your hours
+3. Sends a slack notification on the status of the job
+4. Profit
---
+++ tests/example.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+
+test("Monkey go brrrr", async ({ page }) => {
+  const { USERNAME, PASSWORD, WEEKLY_HOURS } = process.env;
+
+  // Log in
+  await page.goto("https://rm.smartsheet.com/si");
+
+  await page.getByLabel("Email").type(USERNAME!);
+  await page.getByText("Continue", { exact: true }).click();
+
+  const passwordInput = page.getByLabel("Password", { exact: true });
+  await passwordInput.waitFor({ state: "visible" });
+  await passwordInput.type(PASSWORD!);
+  await page.getByText("Sign in", { exact: true }).click();
+
+  // Goto time sheet
+  await page.getByText("Time & Expenses").click();
+  await page.waitForTimeout(1000); // Load data
+
+  const buttons = await page.getByRole("button", { name: /Confirm/ }).all();
+
+  for (const button of buttons) {
+    await button.click();
+  }
+
+  const hours = await page.textContent(
+    ".tk-time-tracker-footer .tk-time-tracker-row-summary .tk-confirmed"
+  );
+
+  expect(hours).toContain(WEEKLY_HOURS);
+
+  // Submit hours
+  await page.getByText("Submit for Approval", { exact: true }).click();
+});
